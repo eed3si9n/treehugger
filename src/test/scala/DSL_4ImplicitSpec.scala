aead2f6055da74e3cc1dcf8651ec184bca1fadc4
e.e d3si9n <eed3si9n@gmail.com>
2012-01-28 06:18:08 -0500

    speced implicits

---
+++ src/test/scala/DSL_4ImplicitSpec.scala
@@ -0,0 +1,24 @@
+import org.specs2._
+
+class DSL_4ImplicitSpec extends DSLSpec { def is = sequential                 ^
+  "This is a specification to check Treehugger DSL"                           ^
+                                                                              p^
+  "Implicit members are written as"                                           ^
+      """`VAL(sym|"x", typ)` withFlags(Flags.IMPLICIT)"""                     ! implicit1^
+                                                                              p^
+  "Implicit parameters are written as"                                        ^
+      """`withParams(VAL(sym|"x", typ) withFlags(Flags.IMPLICIT))`"""         ! implicit2^                                                                            
+                                                                              end
+  
+  import treehugger._
+  import definitions._
+  import treehuggerDSL._
+                                                                             
+  def implicit1 =
+    (VAL("x", sym.Addressable) withFlags(Flags.IMPLICIT): Tree) must print_as(
+      "implicit val x: Addressable")
+  
+  def implicit2 =
+    (DEF("put", UnitClass) withParams(PARAM("x", IntClass) withFlags(Flags.IMPLICIT)) := UNIT) must print_as(
+    "def put(implicit x: Int): Unit = ()")
+}
