76ec5de961ec017faba975223c183207ab14f1c4
e.e d3si9n <eed3si9n@gmail.com>
2012-08-26 20:23:25 -0400

    making MacroBridge generic

--- bridge/src/main/scala/treehugger/MacroBridge.scala
+++ bridge/src/main/scala/treehugger/MacroBridge.scala
@@ -3,7 +3,7 @@ package treehugger
 import scala.reflect.macros.Context
 import treehugger.forest._
 
-case class MacroBridge(context: Context) {
+case class MacroBridge[C <: Context](context: C) {
   val bridge = new Bridge[context.universe.type] {
     def universe = context.universe
     protected def doTransformMods(mods: Modifiers): u.Modifiers =
